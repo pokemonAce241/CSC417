82f62dbf0405d3ecb007f9dc83a98c4d6fb4c4ed
pokemonAce241 <46904793+pokemonAce241@users.noreply.github.com>
2019-03-05 14:09:46 -0500

    Update oo1d.lisp

--- oo1d.lisp
+++ oo1d.lisp
@@ -292,7 +292,7 @@ TODO 2c. Show the output from the following test
     (print `(polymorphism ,sum))))
 
 ; to run, uncomment the following
-(polymorphism)
+'(polymorphism)
 
 #|
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
